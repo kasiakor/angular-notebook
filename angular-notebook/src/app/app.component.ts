25395db3cafe28afb5a6d3c1e4317080fb682375
kasia.kordys <kasia.kordys@gmail.com>
2025-11-16 20:57:56 +0200

    add header component

--- angular-notebook/src/app/app.component.ts
+++ angular-notebook/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent],
 })
 export class AppComponent {
   title: string = 'My Notebook';
